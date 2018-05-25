6e22d8f314c069fd65570809f309656d613acb29
Cole Faust <colecfaust@gmail.com>
2018-05-24 18:49:30 -0700

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float sensitivityX = 1.0f;
+    public float sensitivityY = 1.0f;
+    public float moveSpeed = 1.0f;
+    float rotationY = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*moveSpeed, 0, Input.GetAxis("Vertical")*moveSpeed));
+
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, -89, 89);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        print(rotationY);
+	}
+}
