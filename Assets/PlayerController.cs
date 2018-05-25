170bf9c86aa21018b56ab6f8d0fc33dcee1db656
Cole Faust <colecfaust@gmail.com>
2018-05-24 20:49:21 -0700

    started ability to place platforms

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,11 @@ public class PlayerController : MonoBehaviour {
     public float sensitivityX = 1.0f;
     public float sensitivityY = 1.0f;
     public float moveSpeed = 1.0f;
-    float rotationY = 0.0f;
+    private float rotationY = 0.0f;
+    public GameObject platformTemplate;
+    private GameObject currentPlatform = null;
+    private Vector3 platformStart;
+    private bool drawingPlatform = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        Vector3 lookedAtPoint = transform.position + transform.rotation * new Vector3(0, 0, 3);
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -24,6 +29,24 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             Cursor.lockState = CursorLockMode.Locked;
+
+            if (!drawingPlatform)
+            {
+                platformStart = lookedAtPoint;
+                currentPlatform = Instantiate(platformTemplate);
+                currentPlatform.transform.position = lookedAtPoint;
+                drawingPlatform = true;
+            } else {
+                drawingPlatform = false;
+            }
+        }
+        if(drawingPlatform) {
+            currentPlatform.transform.position = new Vector3((platformStart.x + lookedAtPoint.x) / 2,
+                                                             (platformStart.y + lookedAtPoint.y) / 2,
+                                                             (platformStart.z + lookedAtPoint.z) / 2);
+            Vector3 temp = currentPlatform.transform.localScale;
+            temp.z = (lookedAtPoint - platformStart).magnitude;
+            currentPlatform.transform.localScale = temp;
         }
         if (Cursor.lockState == CursorLockMode.Locked) {
             transform.Translate(new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed));
