ec7fd9417ddcfb0e3e814e49e5c4ffedc3adfb0c
Derek <derek_kwong8@yahoo.com>
2018-06-06 15:41:13 -0700

    added velocity, angle of contact, and contact point detection

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour {
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 vel = rb.velocity;
+        Vector3 normal = collision.contacts[0].normal;
+        print("angle: " + Vector3.Angle(vel, normal));
+        print("contact point: " + collision.contacts[0].point);
+        print("velocity: " + vel);
+    }
+}
