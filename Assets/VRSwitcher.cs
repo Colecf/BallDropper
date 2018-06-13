a05833d8412fff639036027177b59cbc84d7b5fc
Cole Faust <colecfaust@gmail.com>
2018-06-13 15:37:37 -0700

    Added -vr option to launch vr

---
+++ Assets/VRSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VRSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] args = System.Environment.GetCommandLineArgs();
+        //string[] args = { "mygame.exe", "-vr"};
+        bool inVr = false;
+        foreach(string arg in args)
+        {
+            if(arg == "-vr")
+            {
+                inVr = true;
+                SceneManager.LoadScene("VR");
+                break;
+            }
+        }
+        if(!inVr)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
